e8c75ec7c38f415ce418bb0973be43cc178a4e94
mathikgo <35647175+mathikgo@users.noreply.github.com>
2025-11-23 18:26:13 +1100

    Add files via upload
    Day1_Nov152025

---
+++ Day1_15Nov25/assignments/03-primitivedatatypes-practice.js
@@ -0,0 +1,19 @@
+let firstName = "Pooja";
+console.log("This is the value of firstName : ", firstName);
+console.log("This is the type of firstName :", typeof firstName);
+
+let companyName = "TestLeaf";
+console.log("This is the value of companyName: ", companyName);
+console.log("This is the type of companyName :", typeof companyName);
+
+let mobileNumber = 919842562279;
+console.log("This is the value of mobileNumber : ", mobileNumber);
+console.log("This is the type of mobileNumber :", typeof mobileNumber);
+
+let isAutomation = true;
+console.log("This is the value of isAutomation : ", isAutomation);
+console.log("This is the type of isAutomation :", typeof isAutomation);
+
+let hasPlaywright
+console.log("This is the value of hasPlaywright : ", hasPlaywright);
+console.log("This is the type of hasPlaywright :", typeof hasPlaywright);
