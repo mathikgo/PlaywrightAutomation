bf139737c1ea0478a37cd9dfa69f54eca6ce8e54
mathikgo <35647175+mathikgo@users.noreply.github.com>
2025-11-23 18:27:42 +1100

    Update 03-primitivedatatypes-practice.js

--- Day1_15Nov25/assignments/03-primitivedatatypes-practice.js
+++ Day1_15Nov25/assignments/03-primitivedatatypes-practice.js
@@ -17,3 +17,28 @@ console.log("This is the type of isAutomation :", typeof isAutomation);
 let hasPlaywright
 console.log("This is the value of hasPlaywright : ", hasPlaywright);
 console.log("This is the type of hasPlaywright :", typeof hasPlaywright);
+
+
+
+/*Learning Objective:
+Learn primitive data types in JavaScript
+Expected Completion Time:
+Best Case: 10 minutes
+Average Case: 15 minutes
+Assignment Details:
+Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`,
+including an uninitialized variable.
+Assignment Requirements:
+Create the following variables using let (not using var) and check their typeOf
+a) firstName
+b) companyName
+c) mobileNumber
+d) isAutomation
+e) hasPlaywright (do not assign)
+Print and confirm the values and data types
+Hints to Solve:
+Focus on initializing variables with different values, including a string, number, boolean, and leave one
+variable undefined to practice with `typeof`.
+Expected Outcome:
+Upon completion, you should be able to:
+- Grasp the concepts of different data types in JavaScript*/
